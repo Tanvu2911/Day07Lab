76496835415375698543b748efac35eed5c82717
Tanvu2911 <vuviettan29112005@gmail.com>
2025-10-03 21:01:31 +0700

    Thêm toàn bộ code của project

---
+++ Day07Lab/Models/KhachHang.cs
@@ -0,0 +1,31 @@
+﻿using NuGet.Protocol.Plugins;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day07Lab.Models;
+
+public partial class KhachHang
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="không được để trống mã khách hàng")]
+    public string? MaKhachHang { get; set; }
+
+    [Required(ErrorMessage = "không được để trống họ tên")]
+    public string? HoTenKhachHang { get; set; }
+    [Required(ErrorMessage = "không được để trống email")]
+    public string? Email { get; set; }
+    [Required(ErrorMessage = "không được để trống mật khẩu")]
+    public string? MaKhau { get; set; }
+    [Required(ErrorMessage = "không được để trống điện thọai")]
+    public string? DienThoai { get; set; }
+    [Required(ErrorMessage = "không được để trống địa chỉ")]
+    public string? DiaChi { get; set; }
+    [Required(ErrorMessage = "không được để trống Ngày đăng ký")]
+    public DateTime? NgayDangKy { get; set; }
+    [Required(ErrorMessage = "không được để trống trạng thái")]
+    public byte? TrangThai { get; set; }
+
+    public virtual ICollection<HoaDon> HoaDons { get; set; } = new List<HoaDon>();
+}
---
+++ Day07Lab/Models/SanPham.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day07Lab.Models;
+
+public partial class SanPham
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="phải nhập Mã sản phẩm")]
+    public string? MaSanPham { get; set; }
+    [Required(ErrorMessage = "phải nhập tên sản phẩm")]
+
+    public string? TenSanPham { get; set; }
+    [Required(ErrorMessage = "phải nhập hình ảnh")]
+
+    public string? HinhAnh { get; set; }
+    [Required(ErrorMessage = "phải nhập số lượng")]
+
+    public int? SoLuong { get; set; }
+    [Required(ErrorMessage = "phải nhập đơn giá")]
+    public double? DonGia { get; set; }
+    [Required(ErrorMessage = "phải nhập mã loại")]
+    public int? MaLoai { get; set; }
+    [Required(ErrorMessage = "phỉa nhập trạng thái")]
+    public byte? TrangThai { get; set; }
+
+    public virtual ICollection<CtHoaDon> CtHoaDons { get; set; } = new List<CtHoaDon>();
+
+    public virtual LoaiSanPham? MaLoaiNavigation { get; set; }
+}
